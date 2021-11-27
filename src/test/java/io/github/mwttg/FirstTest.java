9c4ba8afb8f14c377f4a17f669e8f2beaf31887e
Marco Wittig <Marco-Wittig@gmx.de>
2021-11-27 19:52:28 +0100

    Add Checkstyle Plugin and reformat code
      - Build now breaks, if format errors

--- src/test/java/io/github/mwttg/FirstTest.java
+++ src/test/java/io/github/mwttg/FirstTest.java
@@ -1,14 +1,14 @@
 package io.github.mwttg;
 
-import org.testng.annotations.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
+
+import org.testng.annotations.Test;
 
 public class FirstTest {
 
- @Test
- public void testAdd3() {
-  final int actual = First.add3(7);
-  assertThat(actual).isEqualTo(10);
- }
+  @Test
+  public void testAdd3() {
+    final int actual = First.add3(7);
+    assertThat(actual).isEqualTo(10);
+  }
 }
