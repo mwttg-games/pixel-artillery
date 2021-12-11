d2bd7f8fe944780b1f2f2e7804bcda4211ffbbbf
Marco Wittig <Marco-Wittig@gmx.de>
2021-12-11 15:40:01 +0100

    Fix Bug in VertexArrayObject
    The Problem was VertexBufferObjects got created before the
    VertexArrayObject. Now the order has changed.

--- src/main/java/io/github/mwttg/tileoperator/core/VertexArrayObject.java
+++ src/main/java/io/github/mwttg/tileoperator/core/VertexArrayObject.java
@@ -20,6 +20,10 @@ public final class VertexArrayObject {
    * @return the OpenGL id
    */
   public static int create(final float[] vertices, final float[] textureCoordinates) {
+    final var id = GL40.glGenVertexArrays();
+    CleanUpUtilities.addVertexArrayObjectId(id);
+    GL40.glBindVertexArray(id);
+
     final var vertexVboId = VertexBufferObject.create(vertices, GL40.GL_STATIC_DRAW);
     GL40.glBindBuffer(GL40.GL_ARRAY_BUFFER, vertexVboId);
     GL40.glVertexAttribPointer(0, 3, GL40.GL_FLOAT, false, 0, 0);
@@ -27,10 +31,6 @@ public final class VertexArrayObject {
     final var uvVboId = VertexBufferObject.create(textureCoordinates, GL40.GL_STATIC_DRAW);
     GL40.glBindBuffer(GL40.GL_ARRAY_BUFFER, uvVboId);
     GL40.glVertexAttribPointer(1, 2, GL40.GL_FLOAT, false, 0, 0);
-
-    final var id = GL40.glGenVertexArrays();
-    CleanUpUtilities.addVertexArrayObjectId(id);
-    GL40.glBindVertexArray(id);
 
     return id;
   }
